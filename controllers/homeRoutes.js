ad74e3973e0b17d041651fea56f703e26754a122
hrcoop16 <hrcoop16@gmail.com>
2022-10-13 19:54:43 -0500

    added js files for pages, added login page and signup page

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,29 +11,34 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get('/stats', async (req, res) => {
+router.get("/stats", async (req, res) => {
   try {
-    res.render('personalStats');
-  }
-  catch (err) {
+    res.render("personalStats");
+  } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
-router.get('/scores', async (req, res) => {
+router.get("/scores", async (req, res) => {
   try {
-    res.render('highScores');
-  }
-  catch (err) {
+    res.render("highScores");
+  } catch (err) {
     res.status(500).json(err);
   }
-})
+});
+
+router.get("/signup", async (req, res) => {
+  try {
+    res.render("signupPage");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get("/login", async (req, res) => {
   try {
-    res.render('signupPage');
-  }
-  catch (err) {
+    res.render("loginPage");
+  } catch (err) {
     res.status(500).json(err);
   }
 });
