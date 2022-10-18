02d9e644ff2a4cca56666d841dcda43e51aa0916
hrcoop16 <hrcoop16@gmail.com>
2022-10-17 19:56:44 -0500

    added back buttons to login and sign up

--- public/js/redirects.js
+++ public/js/redirects.js
@@ -1,15 +1,11 @@
-if (document.querySelector('#login') !== null) {
-    document
-        .querySelector('#login')
-        .addEventListener('click', function() {
-            window.location.href = '/login';
-        });
+if (document.querySelector("#login") !== null) {
+  document.querySelector("#login").addEventListener("click", function () {
+    window.location.href = "/login";
+  });
 }
 
-if (document.querySelector('#signup') !== null) {
-    document
-        .querySelector('#signup')
-        .addEventListener('click', function() {
-            window.location.href = '/signup';
-        });    
+if (document.querySelector("#signup") !== null) {
+  document.querySelector("#signup").addEventListener("click", function () {
+    window.location.href = "/signup";
+  });
 }
